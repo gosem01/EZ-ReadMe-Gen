8af47f3e3c4e25fe55927c858f8b34e892914333
George S <reneehe@George-Sem-2.local>
2023-02-12 21:59:48 -0800

    add question promopts

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,30 @@
 // TODO: Include packages needed for this application
+import inquirer from "inquirer";
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["What is your project title?", 
+                    "Can you describe your project?", 
+                    "How do you install the project?", 
+                    "How does someone use your project?", 
+                    "What are the contribution guidelines, if any?", 
+                    "Are there any testing instructions?", 
+                    "What license is your project under?", 
+                    "What is your Github username?", 
+                    "What is your email address?"];
+
+// TODO 
+//WHEN I choose a license for my application from a list of options
+//THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+
+//TODO
+//WHEN I enter my GitHub username
+//THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+
+//TODO
+//WHEN I enter my email address
+//THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+//WHEN I click on the links in the Table of Contents
+//THEN I am taken to the corresponding section of the README
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
